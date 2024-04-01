b46cc33fa3f4cd6377933c37bcc278d10abbffd4
dhiaa00 <dhiaatipaza42@gmail.com>
2024-04-01 04:54:27 +0100

    front-end: navbar singletourpage for agency

--- front-end/src/components/agency/navbar/AgencyNavbar.jsx
+++ front-end/src/components/agency/navbar/AgencyNavbar.jsx
@@ -1,20 +1,28 @@
-import React from "react";
+import "./agencyNavbar.css";
+import React, { useState } from "react";
+import MenuIcon from "../../../SVGs/menu icon.svg";
+import HelpIcon from "../../../SVGs/help.svg";
 
 const AgencyNavbar = () => {
-  const [displayMenu, setDisplayMenu] = useState(false);
+  const [displayMenu] = useState(false);
   return (
-    <div className="agency-navbar">
-      <div className="agency-navbar__logo"></div>
-      <div className="agency-navbar__links">
-        {displayMenu ? (
-          <div className="agency-navbar__menu">{/* waiting for design */}</div>
-        ) : (
-          // svg element for the menu icon
-          <svg path="../../../SVGs/menu icon.svg"></svg>
-        )}
+    <div className="agency-navbar-container">
+      <div className="agency-navbar">
+        <div className="agency-navbar__logo"></div>
+        <div className="agency-navbar__links">
+          <p>Menu</p>
+          {displayMenu ? (
+            <div className="agency-navbar__menu">
+              {/* waiting for design */}
+            </div>
+          ) : (
+            // svg element for the menu icon
+            <img src={MenuIcon}></img>
+          )}
+        </div>
+        {/* help icon */}
+        <img src={HelpIcon}></img>
       </div>
-      {/* help icon */}
-      <svg path="../../../SVGs/help.svg"></svg>
     </div>
   );
 };
